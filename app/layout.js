6d28d7d3203b705454a1a6d2fc52658478dfa232
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-11-21 10:03:19 +0400

    add tracking code

--- app/layout.js
+++ app/layout.js
@@ -52,6 +52,20 @@ export default function RootLayout({ children }) {
           }}
         />
         {/* End Meta Pixel Code */}
+
+        <Script
+          id="ms-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "u9fgwk7zl5");
+            `,
+          }}
+        />
       </head>
             <body>
                 {children}
